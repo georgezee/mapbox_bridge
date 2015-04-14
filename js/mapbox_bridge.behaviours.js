d6f439b951ff103f7dae8427052e1fb2f6afeb30
Victor Künzig <victor@amazeelabs.com>
2015-04-14 10:50:03 +0000

    UBX-155: adding fieldmatching

--- js/mapbox_bridge.behaviours.js
+++ js/mapbox_bridge.behaviours.js
@@ -7,11 +7,39 @@
    */
   Drupal.behaviors.mapboxBridge = {
     attach: function (context, setting) {
+      if (typeof L != 'undefined' && $('#map', context).length) {
+        $('#map', context).once('mapbox-bridge', function(){
+          // access token for mapbox
+          L.mapbox.accessToken = setting.mapboxBridge.publicToken;
 
-      L.mapbox.accessToken = setting.mapboxBridge.publicToken;
-      var map = L.mapbox.map('map', setting.mapboxBridge.mapId);
+          // Load Mapbox with supplied ID
+          Drupal.Mapbox.map = L.mapbox.map('map', setting.mapboxBridge.mapId);
 
+          // Wait till Mapbox is loaded
+          Drupal.Mapbox.map.on('load', function() {
+            if (typeof setting.mapboxBridge.data != 'undefined') {
+              var data = $.parseJSON(setting.mapboxBridge.data);
+
+              // add markers
+              $.each(data, function(index, markerData){
+                Drupal.behaviors.mapboxBridge.addMarker(markerData);
+              });
+            }
+          });
+
+        });
+
+      }
+    },
+
+    /**
+    * Add marker to the map
+    * */
+    addMarker: function(markerData) {
+      L.marker([markerData.lat, markerData.lon]).addTo(Drupal.Mapbox.map);
     }
+
+    // end Drupal.behaviors.mapboxBridge
   };
 
 })(jQuery);
