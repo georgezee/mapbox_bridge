b4f39746b7b95c31ee9ed7f677537772757633d2
Lucian NEAG <luxian.m@gmail.com>
2015-05-04 10:47:05 -0700

    UBX-193 Updated big chunk of code to support new style of mapping: field attached on taxonomy term for custom marker type and custom icons upload

--- modules/mapbox_views/views/mapbox_views_area_builder.php
+++ modules/mapbox_views/views/mapbox_views_area_builder.php
@@ -28,6 +28,18 @@ class MapboxViewsAreaBuilder {
   private $markerTypeField = '';
 
   /**
+   * @var string
+   *  Symbol name field (list, list_text)
+   */
+  private $fieldSymbolName = '';
+
+  /**
+   * @var string
+   *  Icon upload field (image,file)
+   */
+  private $fieldSymbolIcon = '';
+
+  /**
    * Class constructor
    *
    * @param view $view
@@ -38,33 +50,34 @@ class MapboxViewsAreaBuilder {
    *  Geofield name
    * @param string $markerTypeField
    *  Taxonomy reference field used to get marker type
+   * @param string $symbolName
+   *  Field name used to store symbol name when standar one is used
+   * @param string $symbolIcon
+   *  Field name used to upload custom icons
    */
-  public function __construct(view $view, $mapboxId, $geofield, $markerTypeField = '') {
+  public function __construct(view $view, $mapboxId, $geofield, $markerTypeField = '', $symbolName = '', $symbolIcon = '') {
     $this->view = $view;
     $this->mapboxId = $mapboxId;
     $this->geofield = $geofield;
     $this->markerTypeField = $markerTypeField;
+    $this->fieldSymbolIcon = $symbolIcon;
+    $this->fieldSymbolName = $symbolName;
   }
 
   /**
    * Get the map
-   *
-   * @param array $markersTypeRemap
-   *  Remap markertypes value returned from database
    *
    * @return string
    *
    * @throws \Exception
    * @throws \PDOException
    */
-  public function getMap($markersTypeRemap = array()) {
+  public function getMap() {
     $allViewResults = $this->getAllViewResults();
     $allEntityIds = $this->extractEntityIds($allViewResults);
-    $mapPins = $this->extractPinsPosition($allEntityIds);
-    if ($markersTypeRemap !== array()) {
-      $this->remapPinTypes($mapPins, $markersTypeRemap);
-    }
-    return mapbox_bridge_render_map($this->mapboxId, $mapPins, 'views');
+    $mapMarkers = $this->extractMarkersInfo($allEntityIds);
+    $mapMarkers = $this->processMarkersSymbol($mapMarkers);
+    return mapbox_bridge_render_map($this->mapboxId, $mapMarkers, 'views');
   }
 
   /**
@@ -148,23 +161,35 @@ class MapboxViewsAreaBuilder {
    *
    * @throws \PDOException
    */
-  private function extractPinsPosition($entityIds) {
+  private function extractMarkersInfo($entityIds) {
     // Start with geofield data
     $q = db_select("field_data_{$this->geofield}", 'g');
     $q->addField('g', "{$this->geofield}_lat", 'lat');
     $q->addField('g', "{$this->geofield}_lon", 'lon');
+    $q->addField('g', "entity_id", 'nid');
 
     $q->condition('g.entity_type', 'node');
     $q->condition('g.entity_id', $entityIds, 'IN');
 
     // Add the mapping field
-    if ($this->markerTypeField) {
+    if ($this->markerTypeField && ($this->fieldSymbolName || $this->fieldSymbolIcon)) {
       $join_conditions = 'm.entity_type = g.entity_type AND m.entity_id = g.entity_id';
-      $q->leftJoin("field_data_{$this->markerTypeField}", 'm', $join_conditions);
-      $q->leftJoin('taxonomy_term_data', 'td', 'td.tid = m.' . "{$this->markerTypeField}_tid");
-      $q->addField('td', 'name', 'type');
-      // @todo: left join marker type field from term entity instead of tid
-      // ...
+      $q->leftJoin("field_data_{$this->markerTypeField}", "m", $join_conditions);
+      //$q->leftJoin('taxonomy_term_data', 'td', 'td.tid = m.' . "{$this->markerTypeField}_tid");
+
+      if ($this->fieldSymbolName) {
+        $q->leftJoin("field_data_{$this->fieldSymbolName}", "fsn", "fsn.entity_type = 'taxonomy_term' AND fsn.entity_id = m.{$this->markerTypeField}_tid");
+        $q->addField('fsn', "{$this->fieldSymbolName}_value", 'type');
+      }
+      else {
+        $q->addExpression("''", 'symbolName');
+      }
+
+      if ($this->fieldSymbolIcon) {
+        $q->leftJoin("field_data_{$this->fieldSymbolIcon}", "fsi", "fsi.entity_type = 'taxonomy_term' AND fsi.entity_id = m.{$this->markerTypeField}_tid");
+        $q->leftJoin("file_managed", "fm", "fm.fid = fsi.{$this->fieldSymbolIcon}_fid");
+        $q->addField("fm", "uri", "icon");
+      }
     }
     else {
       $q->addExpression('NULL', 'type');
@@ -173,28 +198,14 @@ class MapboxViewsAreaBuilder {
     return $q->execute()->fetchAll();
   }
 
-  /**
-   * Remap pin types
-   *
-   * @param array $mapPins
-   * @param array $markersTypeRemap
-   */
-  private function remapPinTypes(array &$mapPins, array $markersTypeRemap) {
-    $keyedRemapArray = array();
-    foreach($markersTypeRemap as $remap) {
-      if (!empty($remap['marker_type']) && !empty($remap['taxonomy_term'])) {
-        $keyedRemapArray[$remap['taxonomy_term']] = $remap['marker_type'];
+  private function processMarkersSymbol(array $markers) {
+    foreach($markers as $marker) {
+      if (!isset($marker->type)) {
+        $marker->type = '';
       }
-    }
 
-    foreach($mapPins as $i => $pin) {
-      if (isset($keyedRemapArray[$pin->type])) {
-        $mapPins[$i]->type = $keyedRemapArray[$pin->type];
-      }
-      else {
-        $mapPins[$i]->type = '';
-      }
+      $marker->icon = isset($marker->icon) ? file_create_url($marker->icon) : '';
     }
+    return $markers;
   }
-
 }
